1899533012019eb9ae923126f4520b94d2cb6ddd
facoe <55077903+facoe@users.noreply.github.com>
2025-04-01 11:01:44 -0600

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,12 @@
+local areas = {
+    {x = 1818.00, y = 2594.00, z = 45.72, radius = 40.0},
+}
+
 Citizen.CreateThread(function()
-    while (true) do
-		ClearAreaOfPeds(1818.00, 2594.00, 45.72, 40.0, 1)
+    while true do
+        for _, area in ipairs(areas) do
+            ClearAreaOfPeds(area.x, area.y, area.z, area.radius, 1)
+        end
         Citizen.Wait(0)
     end
 end)
